449c0be036dab3557e8e3874a7cb103365ffc8fd
zahur76 <zahurmeerun@hotmail.com>
2020-11-08 16:18:28 +0000

    Add all app.cofigurations required for project

--- app.py
+++ app.py
@@ -2,12 +2,20 @@ import os
 from flask import (
     Flask, render_template,
     redirect, request, session, url_for)
-from flask_pymongo import flask_Pymongo 
+from flask_pymongo import flask_PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+# Used to connect to mongodb
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+# Used for session cookie
+app.secret_key = os.environ.get("SECRET_KEY")
+# creating mongodb using ["MONGO_URI"]
+mongo = PyMongo(app)
 
 
 @app.route("/")
