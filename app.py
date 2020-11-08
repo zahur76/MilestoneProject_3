2cce2ee079a6e808b529a736af8078c0df7a488a
zahur76 <zahurmeerun@hotmail.com>
2020-11-08 16:33:09 +0000

    Add app route for items page

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import (
     Flask, render_template,
     redirect, request, session, url_for)
-from flask_pymongo import flask_PyMongo
+from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
@@ -20,12 +20,11 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def all_items():
-    return "Hello world"
+    mongo.db.items.find()
+    return render_template("/items")
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-
-
