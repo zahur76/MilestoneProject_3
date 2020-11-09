ec7be501ada37253472cf5bf341912eaa73016df
zahur76 <zahurmeerun@hotmail.com>
2020-11-09 09:16:39 +0000

    Fix issue where modal closes with invalid input using Javascript

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
+    /* Nav bar collpase activation */
     $('.sidenav').sidenav();
-  });
+    /* Modal activation */
+    $('.modal').modal();   
+    
+    /* Code to ensure modal does not close if fields are invalid */
+    $(".submit").click(function(){
+        let username_attr = $("#username").attr("class");
+        let password_attr = $("#username").attr("class");
+        console.log(attribute)
+        if(username_attr=="validate valid" || password_attr=="validate valid"){
+            $(".submit").addClass("modal-close")
+        }        
+    })
+
+})
+
+
+    
