f84633f566b17402a6983c63f30ef62512bbdc56
zahur76 <zahurmeerun@hotmail.com>
2020-11-09 14:22:49 +0000

    Add registration app route function to app.py file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import (
-    Flask, render_template,
+    Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -23,6 +23,30 @@ mongo = PyMongo(app)
 def all_items():
     items = mongo.db.items.find()
     return render_template("items.html", items=items)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # Find a match in exsiting user database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        # If a match is found and existing_user returns a username
+        if existing_user:
+            flash("Username already exists!")
+            return redirect(url_for("all_items"))
+        # If no match is found
+        else:
+            # Store username/password into users database
+            register = {"username": request.form.get("username").lower(),
+                        "password": generate_password_hash(
+                        request.form.get("password"))}
+            mongo.db.users.insert_one(register)
+            # Add username into session cookie
+            session["user"] = request.form.get("username").lower()
+            flash("You have been registered!")
+            return redirect(url_for("all_items"))
+
 
 
 if __name__ == "__main__":
